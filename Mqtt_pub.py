a6e2eeaafc9fe475dd756b54d5fe243440110e53
IndhiraBusanaboina07 <123065939+IndhiraBusanaboina07@users.noreply.github.com>
2023-01-19 17:18:53 +0530

    Update Mqtt_pub.py

--- Mqtt_pub.py
+++ Mqtt_pub.py
@@ -11,10 +11,9 @@ while True:
     try:
      client.connect(broker,port)
      print('Broker connected')
-     msg = "{'Humidity':"+str(random.randint(20,100))+",'Temperature':"+str(random.randint(20,40))+"}"
+     msg = '{"Humidity":'+str(random.randint(20,100))+',"Temperature":'+str(random.randint(20,40))+'}'
      client.publish('sacet/a11',msg)
      time.sleep(4)
 
     except:
      print('Broker Connection Failure')
-
